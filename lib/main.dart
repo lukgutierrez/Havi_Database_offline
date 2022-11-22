a187167c0ec4e70ea39d859c9c647c5f80b91b85
Luk Gutierrez <lucianogutierrezagustin@gmail.com>
2022-11-22 15:02:53 -0300

    createaplication

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,43 @@
 import 'package:flutter/material.dart';
+import 'package:hive_database_proyect/players.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:hive_generator/hive_generator.dart';
 
-void main() => runApp(MyApp());
+Future<void> main() async {
+  await Hive.initFlutter();
+  Hive.registerAdapter(PeopleAdapter());
+  runApp(MyApp());
+}
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
+    final TextEditingController controller = TextEditingController();
+    @override
+    void dispose() {
+      controller.dispose();
+      super.dispose();
+    }
+
     return MaterialApp(
       title: 'Material App',
       home: Scaffold(
-        appBar: AppBar(
-          title: Text('Material App Bar'),
-        ),
-        body: Center(
-          child: Container(
-            child: Text('Hello World'),
+          appBar: AppBar(
+            title: Text('Material App Bar'),
           ),
-        ),
-      ),
+          body: Column(
+            children: [
+              TextField(
+                controller: controller,
+              ),
+              ElevatedButton(onPressed: () {}, child: Text("Hello"))
+            ],
+          )),
     );
   }
 }
